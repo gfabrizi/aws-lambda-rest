73e9ea2f167f28b95ae26bca476c2a05f08581ff
Gianluca Fabrizi <info@gianlucafabrizi.it>
2022-04-25 19:31:59 +0200

    Enpoint CRUD funzionanti

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FUNCTION_NAME=$(< package.json grep \"name\": | awk '{print $2}' | tr -d '",')
+
 rm -f deploy.zip
 zip -r deploy.zip ./* -x deploy.sh -x ./*.zip
-aws lambda --region eu-south-1 update-function-code --function-name function-url-demo --zip-file fileb://deploy.zip
+aws lambda --region eu-south-1 update-function-code --function-name $FUNCTION_NAME --zip-file fileb://deploy.zip
--- index.js
+++ index.js
@@ -21,28 +21,73 @@ let users = [
     }
 ];
 
-app.get('/users', (res) => {
+app.get('/users', (req, res) => {
     res.json(users, 200);
 });
 
-app.get('/users/{id}', (res, id) => {
+// Create
+app.post('/users', (req, res) => {
+    // In un mondo ideale, qui faremmo una validazione dei dati in input
+    const ids = users.map(user => {
+        return user.id;
+    });
+
+    const maxId = Math.max(...ids);
+
+    let user = {
+        'id': maxId + 1,
+        'firstname': req.body.firstname,
+        'lastname': req.body.lastname,
+    }
+
+    users.push(user);
+
+    res.json(user, 200);
+});
+
+// Retrieve
+app.get('/users/{id}', (req, res, id) => {
     let user = users.find((entry) => {
         return entry.id === parseInt(id);
     });
 
     if (!user) {
-        res.json('User not found', 404);
+        res.json(`User ${id} not found`, 404);
     } else {
         res.json(user, 200);
     }
 });
 
-app.get('/test', (res) => {
-    res.json('Risposta da /test', 200);
+// Update
+app.put('/users/{id}', (req, res, id) => {
+    // In un mondo ideale, qui faremmo una validazione dei dati in input
+    let updateIndex = users.map(user => user.id).indexOf(parseInt(id));
+
+    if (updateIndex !== -1) {
+        users[updateIndex].firstname = req.body.firstname;
+        users[updateIndex].lastname = req.body.lastname;
+        res.json(users[updateIndex], 200);
+    } else {
+        res.json(`User ${id} not found`, 404);
+    }
+});
+
+// Delete
+app.delete('/users/{id}', (req, res, id) => {
+    // In un mondo ideale, qui faremmo una validazione dei dati in input
+    let removeIndex = users.map(user => user.id).indexOf(parseInt(id));
+
+    if (removeIndex !== -1) {
+        res.json(users[removeIndex], 200);
+        users.splice(removeIndex, 1);
+    } else {
+        res.json(`User ${id} not found`, 404);
+    }
 });
 
 let handler = async (event, context) => {
-    app.handle(event.requestContext);
+    res.reset();
+    app.handle(event);
 
     return res.getResponse();
 };
